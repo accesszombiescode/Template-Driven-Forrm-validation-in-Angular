ce7b85969f04937b06c2af88f7018ab388f1f873
AccessZombiesCode <learnforestcode@gmail.com>
2022-05-15 13:40:55 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {path:'',pathMatch:'full',redirectTo:'signin'},
+  {path:'signin',component:SigninComponent},
+  {path:'register',component:RegisterComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
